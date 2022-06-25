f305049334e800cc0946e885fb585d77444d4c9c
pramod ukkali <pramod.ukkali@instavans.com>
2022-06-25 17:03:27 +0530

    new changes

--- Controllers/Result.js
+++ Controllers/Result.js
@@ -44,7 +44,9 @@ const getResult = async (query) => {
         else if ((temp1.cie === 'AB' || temp1.cie === 'NE' || temp1.cie === 'A' || temp1.cie === 'B' || temp1.cie === 'C' || temp1.cie === 'D' || temp1.cie === 'F') && (temp1.see === 'AB' || temp1.see === 'NE' || temp1.see.marks === 'A' || temp1.see.marks === 'B' || temp1.see.marks === 'C' || temp1.see.marks === 'D' || temp1.see.marks === 'F')) temp1.total = 0;
         else temp1.total = temp1.cie + temp1.see;
         // temp1.total = temp1.cie + temp1.see;
-        temp1.result = (temp1.cie >= temp1.subject.min_cie_marks && temp1.see >= temp1.subject.min_see_marks && temp1.cie !== 'AB' && temp1.see !== 'NE') ? 'P' : 'F';
+        console.log("result>>>>>", temp1.cie, temp1.subject.min_cie_marks, temp1.see, temp1.subject.min_see_marks);
+        if (temp1.subject.credits !== 1) temp1.result = (temp1.cie >= temp1.subject.min_cie_marks && temp1.see >= temp1.subject.min_see_marks && temp1.cie !== 'AB' && temp1.see !== 'NE') ? 'P' : 'F';
+        else if (temp1.subject.credits === 1) temp1.result = (temp1.cie === 'F') ? 'F' : 'P';
         return temp1;
       });
         // console.log('allMarks>>>>', allMarks);
@@ -54,7 +56,7 @@ const getResult = async (query) => {
       // sort allmarks by subject.sort_criteria
       allMarks.sort((a, b) => a.subject.sort_criteria - b.subject.sort_criteria);
       temp.marks = allMarks;
-      // console.log('allMarks>>>>', allMarks);
+      console.log('allMarks>>>>', temp.marks);
 
       temp.total = allMarks.reduce((acc, curr) => acc + curr.total, 0);
       // console.log('temp.total>>>>', temp.total);
@@ -78,17 +80,18 @@ const getResult = async (query) => {
 
       temp.finalResult = (temp.remarks === 'FAIL') ? 'FAIL' : 'PASS';
       if (student.branch !== 'AU') {
-        if (temp.total >= 280) temp.finalResult = 'DISTINCTION';
-        if (temp.total >= 260 && temp.total < 280) temp.finalResult = 'FIRST CLASS';
+        console.log("result final>>>>", temp.finalResult, temp.remarks, temp.total);
+        if (temp.finalResult !== 'FAIL' && temp.total >= 280) temp.finalResult = 'DISTINCTION';
+        if (temp.finalResult !== 'FAIL' && temp.total >= 260 && temp.total < 280) temp.finalResult = 'FIRST CLASS';
+        console.log("result final>>>>", temp.finalResult, temp.remarks);
       }
       if (student.branch === 'AU') {
-        if (temp.total >= 350) temp.finalResult = 'DISTINCTION';
-        if (temp.total >= 300 && temp.total < 350) temp.finalResult = 'FIRST CLASS';
+        if (temp.finalResult !== 'FAIL' && temp.total >= 350) temp.finalResult = 'DISTINCTION';
+        if (temp.finalResult !== 'FAIL' && temp.total >= 300 && temp.total < 350) temp.finalResult = 'FIRST CLASS';
       }
       return temp;
     }
   });
-  console.log('stude>>>', result[0]);
   return result;
 };
 
